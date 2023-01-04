760fef069acff4ae7eb7ccf2f8a1f078ab4b3164
Filopatir-Mamdouh <filopatirmamdouh@gmail.com>
2023-01-04 21:29:26 +0200

    front end of the login screen

--- WindowsFormsApp1/login.cs
+++ WindowsFormsApp1/login.cs
@@ -17,7 +17,7 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void label2_Click(object sender, EventArgs e)
         {
 
         }
